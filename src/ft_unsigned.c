6e0d39fd86e33bac45bbf56172526f0941405bdb
Mi Hwangbo <mhwangbo@e1z2r10p3.42.us.org>
2018-04-23 17:04:04 -0700

    edited

--- src/ft_unsigned.c
+++ src/ft_unsigned.c
@@ -6,7 +6,7 @@
 /*   By: mhwangbo <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/04/23 14:28:57 by mhwangbo          #+#    #+#             */
-/*   Updated: 2018/04/23 15:42:31 by mhwangbo         ###   ########.fr       */
+/*   Updated: 2018/04/23 17:03:52 by mhwangbo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -54,12 +54,15 @@ int		ft_unsigned(char *buf, va_list args, int *j, const char *format)
 	int				form;
 	t_chars			*chars;
 	int				base;
+	unsigned long long	i;
 
 	form = 0;
 	flags = ft_flags(format, 4, args, &form);
 	chars = ft_chars_malloc();
 	base = ft_unsigned_s(format);
-	chars->str = ft_llitoa_base(ft_un_cv(flags, args), base);
+	i = ft_un_cv(flags, args);
+	printf("INPUT: %llu\n", i);
+	chars->str = ft_llitoa_base(i, base);
 	ft_to_capital(chars, format);
 	ft_flag_app(flags, chars);
 	ft_str_to_buf(chars, buf, &j, 4);

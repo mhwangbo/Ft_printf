771e2d4de20d0c94c2c021f46a7c951d2a105ba2
Mi Hwangbo <mhwangbo@e1z2r10p3.42.us.org>
2018-04-24 16:12:14 -0700

    pointer

--- src/ft_pointer.c
+++ src/ft_pointer.c
@@ -6,7 +6,7 @@
 /*   By: mhwangbo <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/04/19 19:36:12 by mhwangbo          #+#    #+#             */
-/*   Updated: 2018/04/24 15:58:07 by mhwangbo         ###   ########.fr       */
+/*   Updated: 2018/04/24 16:11:49 by mhwangbo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,7 +25,13 @@ int		ft_pointer(va_list args, const char *format, t_numbers *n)
 	while (!ft_strchr("sSpdDioOuUxXcC", format[form]))
 		form++;
 	int_v = (uintmax_t)va_arg(args, void*);
-	chars->str = ft_uitoa_base(int_v, 16);
+	chars->str = ft_llitoa_base(int_v, 16);
+	if (int_v == 0)
+	{
+		chars->str[0] = '0';
+		flags.o_zero = 1;
+		flags.hash = 0;
+	}
 	ft_flag_app(flags, chars);
 	chars->front = ft_strjoin("0x", chars->front);
 	ft_str_to_buf(chars, 1, n);

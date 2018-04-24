1cf3f25cc4ac80ac7c43fe57540087c912f45c40
Mi Hwangbo <mhwangbo@e1z2r10p3.42.us.org>
2018-04-23 17:06:43 -0700

    edited

--- src/ft_unsigned.c
+++ src/ft_unsigned.c
@@ -6,7 +6,7 @@
 /*   By: mhwangbo <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/04/23 14:28:57 by mhwangbo          #+#    #+#             */
-/*   Updated: 2018/04/23 17:05:45 by mhwangbo         ###   ########.fr       */
+/*   Updated: 2018/04/23 17:06:35 by mhwangbo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -61,8 +61,10 @@ int		ft_unsigned(char *buf, va_list args, int *j, const char *format)
 	chars = ft_chars_malloc();
 	base = ft_unsigned_s(format);
 	i = ft_un_cv(flags, args);
-	chars->str = ft_llitoa_base(i, base);
-	printf("ZERO: chars->str = %s\n", chars->str);
+	if (i == 0)
+		chars->str = '0'
+	else
+		chars->str = ft_llitoa_base(i, base);
 	ft_to_capital(chars, format);
 	ft_flag_app(flags, chars);
 	ft_str_to_buf(chars, buf, &j, 4);

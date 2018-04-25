30df6c9cdbca02e9f886e8f913e0f43974a67902
Mi Hwangbo <mhwangbo@e1z2r10p3.42.us.org>
2018-04-24 23:31:43 -0700

    to extract inputs

--- src/ft_printf.c
+++ src/ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: mhwangbo <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/04/08 18:26:02 by mhwangbo          #+#    #+#             */
-/*   Updated: 2018/04/24 22:36:50 by mhwangbo         ###   ########.fr       */
+/*   Updated: 2018/04/24 23:31:07 by mhwangbo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@
 ** 2 = signed decimal
 ** 3 = unsigned octal, unsigned decimal, unsigned hexadecimal (x & X)
 */
-
+/*
 int		ft_printf_send(va_list args, t_numbers *n, const char *format)
 {
 	if (n->spec == 0)
@@ -95,4 +95,14 @@ int		ft_printf(const char *format, ...)
 	i = ft_vsprintf(format, args, &n);
 	va_end(args);
 	return (n.return_i);
+}*/
+
+int		ft_printf(const char *format, ...)
+{
+	va_list arglist;
+
+	va_start(arglist, format);
+	vprintf(format, arglist);
+	va_end(arglist);
+	return (0);
 }

69a39e5a00d0ed352c4f9805c26c3bf29b42293a
Mi Hwangbo <mhwangbo@e1z2r10p3.42.us.org>
2018-04-24 23:23:11 -0700

    character

--- src/ft_char_str.c
+++ src/ft_char_str.c
@@ -6,7 +6,7 @@
 /*   By: mhwangbo <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/04/15 18:11:21 by mhwangbo          #+#    #+#             */
-/*   Updated: 2018/04/24 23:19:09 by mhwangbo         ###   ########.fr       */
+/*   Updated: 2018/04/24 23:22:12 by mhwangbo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,15 +35,16 @@ int		ft_character(va_list args, const char *format, t_numbers *n)
 	int		form;
 
 	form = 0;
-	chars = ft_chars_malloc();
 	flags = ft_flags(format, 1, args, &form);
 	if (flags.length == 4)
 		return (ft_wide_char(args, form, flags, n));
 	else
+	{
+		chars = ft_chars_malloc();
 		chars->str[0] = va_arg(args, int);
+	}
 	if (chars->str[0] == '\0' && flags.width == 0)
 		n->return_i += 1;
-	printf("INPUT: %c\n", chars->str[0]);
 	ft_flag_app(flags, chars);
 	ft_str_to_buf(chars, 1, n);
 	ft_chars_free(chars, flags);

4486ac3a2e69fbd36681967cd293aeab9f9c9b89
Mi Hwangbo <mhwangbo@e1z2r5p7.42.us.org>
2018-04-29 16:58:19 -0700

    testing

--- src/ft_unsigned.c
+++ src/ft_unsigned.c
@@ -6,7 +6,7 @@
 /*   By: mhwangbo <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/04/23 14:28:57 by mhwangbo          #+#    #+#             */
-/*   Updated: 2018/04/29 16:57:19 by mhwangbo         ###   ########.fr       */
+/*   Updated: 2018/04/29 16:58:11 by mhwangbo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -210,7 +210,7 @@ int		ft_unsigned(va_list args, const char *format, t_numbers *n)
 		chars->str = ft_llitoa_base(i, flags.base);
 	ft_flag_app(flags, chars);
 	ft_to_capital(chars, format);
-	ft_str_to_buf(chars, 4, n);
+	ft_str_to_buf(chars, n);
 	free(chars);
 	return (form + 1);
 }
